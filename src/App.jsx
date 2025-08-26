549c671b8a95a73b2ee865f2f0491b392441f61b
Daniel <danielandresogodieme@gmail.com>
2025-08-26 11:53:09 +0000

    feat: display coin cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
 const API_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
@@ -28,7 +29,18 @@ const App = () => {
 
   return (
     <div>
-      <h1>🚀 Hello World!</h1>
+      <h1>🚀 Crypto Dash </h1>
+
+      {loading && <div>Loading...</div>}
+      {error && <div className="error">{error}</div>}
+
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}
+        </main>
+      )}
     </div>
   );
 };
