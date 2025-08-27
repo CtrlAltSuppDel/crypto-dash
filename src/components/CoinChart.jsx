c3ff55293da143cebd65c0b7802109b77f1c6bcd
Daniel <danielandresogodieme@gmail.com>
2025-08-27 16:33:50 +0000

    fix: Date adapter is not provided error

--- src/components/CoinChart.jsx
+++ src/components/CoinChart.jsx
@@ -1,4 +1,5 @@
 import { Line } from "react-chartjs-2";
+import "chartjs-adapter-date-fns";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -35,8 +36,25 @@ const CoinChart = ({ coinId }) => {
         );
         if (!res.ok) throw new Error("Failed to fetch the data");
         const data = await res.json();
-        console.log(data);
-        setChartData(data);
+
+        const prices = data.prices.map((price) => ({
+          x: price[0],
+          y: price[1],
+        }));
+
+        setChartData({
+          datasets: [
+            {
+              label: "Price (USD)",
+              data: prices,
+              fill: true,
+              borderColor: "#007bff",
+              backgroundColor: "rbga(0, 123, 255, 0.1)",
+              pointRadius: 0,
+              tension: 0.3,
+            },
+          ],
+        });
       } catch (error) {
         console.log(error.message);
         setError(error.message);
@@ -46,7 +64,42 @@ const CoinChart = ({ coinId }) => {
     };
     fetchPrices();
   }, [coinId]);
-  return <div>Hello coin chart</div>;
+
+  if (loading) return <p>Loading Chart...</p>;
+
+  return (
+    <div style={{ marginTop: "30px" }}>
+      {!loading && !error && chartData && (
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            plugins: {
+              legend: { display: false },
+              tooltip: { mode: "index", intersect: false },
+            },
+            scales: {
+              x: {
+                type: "time",
+                time: {
+                  unit: "day",
+                },
+                ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: 7,
+                },
+              },
+              y: {
+                ticks: {
+                  callback: (value) => `$${value.toLocaleString()}`,
+                },
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default CoinChart;
